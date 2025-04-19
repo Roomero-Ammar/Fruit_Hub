b261447f326e7bf3a028b34d45b2305cbde5737d
AmmarYaser <ay1513600@gmail.com>
2025-04-19 18:47:41 +0200

    add final appRouter to use it instead of AppRouter() , remove consts from test and main

--- lib/fruit_hub.dart
+++ lib/fruit_hub.dart
@@ -3,8 +3,8 @@ import 'package:fruit_hub/core/helper_functions/app_router.dart';
 import 'package:fruit_hub/core/helper_functions/routes.dart';
 
 class FruitHub extends StatelessWidget {
-  const FruitHub({super.key});
-
+   FruitHub({super.key});
+final AppRouter appRouter = AppRouter();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.SplashView,
-      onGenerateRoute: AppRouter().onGenerateRoute,
+      onGenerateRoute: appRouter.onGenerateRoute,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:fruit_hub/fruit_hub.dart';
 
 void main() {
-  runApp(const FruitHub());
+  runApp( FruitHub());
 }
