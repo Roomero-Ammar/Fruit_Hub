fbc5420dccf64505ff9b28844f2dc39242394b21
AmmarYaser <ay1513600@gmail.com>
2025-04-24 02:16:23 +0200

    add const DotsIndicator

--- lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
@@ -1,6 +1,8 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruit_hub/core/helper_functions/spacing.dart';
+import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/features/on_boarding/presentation/view/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -13,8 +15,15 @@ class OnBoardingViewBody extends StatelessWidget {
         children: [
           // يمكنك تحديد ارتفاع هنا
           SizedBox(
-            height: 800.h, // تحديد ارتفاع مناسب
+            height: 700.h, // تحديد ارتفاع مناسب
             child: OnBoardingPageView(),
+          ),
+          DotsIndicator(
+            dotsCount: 2,
+            decorator: DotsDecorator(
+              activeColor: AppColors.primaryColor,
+              color: AppColors.primaryColor.withValues(alpha: 0.5),
+              ),
           ),
           verticalSpace(10),
           Center(child: Text('OnBoarding View Body')),
