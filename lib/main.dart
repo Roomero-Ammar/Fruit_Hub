e7c6fdbd36c64e55fd181740872037fd76f09c6c
AmmarYaser <ay1513600@gmail.com>
2025-05-02 02:25:33 +0300

    create shared_preferences_singleton and init it in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/fruit_hub.dart';
+import 'core/services/shared_preferences_singleton.dart';
 
-void main() {
+void main() async{
+  await Prefs.init();
   runApp( FruitHub());
 }
