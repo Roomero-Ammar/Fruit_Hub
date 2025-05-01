1ab4384e5b7c245dea249cb6c5e606e4098b5563
AmmarYaser <ay1513600@gmail.com>
2025-05-02 02:34:50 +0300

    solve problem about missing the   WidgetsFlutterBinding.ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:fruit_hub/fruit_hub.dart';
 import 'core/services/shared_preferences_singleton.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
   runApp( FruitHub());
 }
