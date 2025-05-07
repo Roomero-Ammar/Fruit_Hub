9ebf285f871315904179608305cabbef42681bf3
AmmarYaser <ay1513600@gmail.com>
2025-05-07 18:43:57 +0300

    finish Ui of Signup View

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/constants/constants.dart';
+import '../../../../../core/helper_functions/spacing.dart';
+import '../../../../../core/widgets/custom_button.dart';
 import '../../../../../core/widgets/custom_text_field.dart';
 import '../../../../../core/widgets/password_field.dart';
+import 'have_an_account_widget.dart';
 import 'terms_and_conditions.dart';
 
 class SignupViewBody extends StatefulWidget {
@@ -15,37 +18,30 @@ class SignupViewBody extends StatefulWidget {
 class _SignupViewBodyState extends State<SignupViewBody> {
   @override
   Widget build(BuildContext context) {
-    return  SingleChildScrollView(
+    return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
         child: Column(
           children: [
-        const SizedBox(
-                height: 16,
-              ),
-              CustomTextFormField(
-                  
-                
-                  hintText: 'البريد الإلكتروني',
-                  textInputType: TextInputType.emailAddress),
-              const SizedBox(
-                height: 16,
-              ),
-              PasswordField(
-                
-               
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TermsAndConditionsWidget(
-                onChanged: (value) {
-                },
-               
-              ),
-              const SizedBox(
-                height: 30,
-              ),
+            verticalSpace(24),
+             CustomTextFormField(
+                  onSaved: (value) {
+                  },
+                  hintText: 'الاسم كامل',
+                  textInputType: TextInputType.name),
+            verticalSpace(16),
+            CustomTextFormField(
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            verticalSpace(16),
+            PasswordField(),
+            verticalSpace(16),
+            TermsAndConditionsWidget(onChanged: (value) {}),
+            verticalSpace(30),
+            CustomButton(onPressed: (){}, text: 'إنشاء حساب جديد',),
+            verticalSpace(26),
+            HaveAnAccountWidget(),
           ],
         ),
       ),
