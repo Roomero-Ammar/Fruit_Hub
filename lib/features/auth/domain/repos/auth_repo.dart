2887eaad4067f19ce1f736823eae0520a9842361
AmmarYaser <ay1513600@gmail.com>
2025-05-09 02:36:12 +0300

    create UserEntity class and used it in auth repo file as createUserWithEmailAndPassword

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,9 @@
+import 'package:fruit_hub/features/auth/domain/entites/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<UserEntity> createUserWithEmailAndPassword(
+    String email,
+    String password,
+    String name,
+  );
+}
