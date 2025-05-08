717263aad372537df11a4f8299556b6c732e9c5a
AmmarYaser <ay1513600@gmail.com>
2025-05-09 00:55:17 +0300

    initial direbase in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/firebase_options.dart';
 import 'package:fruit_hub/fruit_hub.dart';
 import 'core/services/shared_preferences_singleton.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp( FruitHub());
 }
