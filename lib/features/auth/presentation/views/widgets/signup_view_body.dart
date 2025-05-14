3f2da1f0fff085f7b39919333edd5de0c3308afa
AmmarYaser <ay1513600@gmail.com>
2025-05-15 01:21:15 +0300

    add form validation in signup_view_body and read<SignupCubit>() .createUserWithEmailAndPassword

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/widgets/custom_show_bar.dart';
 
 import '../../../../../core/constants/constants.dart';
 import '../../../../../core/helper_functions/spacing.dart';
 import '../../../../../core/widgets/custom_button.dart';
 import '../../../../../core/widgets/custom_text_field.dart';
 import '../../../../../core/widgets/password_field.dart';
+import '../../cubits/signup_cubit/cubit/signup_cubit.dart';
 import 'have_an_account_widget.dart';
 import 'terms_and_conditions.dart';
 
@@ -16,33 +19,74 @@ class SignupViewBody extends StatefulWidget {
 }
 
 class _SignupViewBodyState extends State<SignupViewBody> {
+
+  final GlobalKey <FormState> formKey = GlobalKey<FormState>();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  // String? email, userName, password;
+
+  late String email, userName, password;
+  late bool isTermsAccepted = false;
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
-        child: Column(
-          children: [
-            verticalSpace(24),
-             CustomTextFormField(
-                  onSaved: (value) {
-                  },
-                  hintText: 'الاسم كامل',
-                  textInputType: TextInputType.name),
-            verticalSpace(16),
-            CustomTextFormField(
-              hintText: 'البريد الإلكتروني',
-              textInputType: TextInputType.emailAddress,
-            ),
-            verticalSpace(16),
-            PasswordField(),
-            verticalSpace(16),
-            TermsAndConditionsWidget(onChanged: (value) {}),
-            verticalSpace(30),
-            CustomButton(onPressed: (){}, text: 'إنشاء حساب جديد',),
-            verticalSpace(26),
-            HaveAnAccountWidget(),
-          ],
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            children: [
+              verticalSpace(24),
+               CustomTextFormField(
+                    onSaved: (value) {
+                      userName = value!;
+                    },
+                    hintText: 'الاسم كامل',
+                    textInputType: TextInputType.name),
+              verticalSpace(16),
+              CustomTextFormField(
+                onSaved: (value) {
+                      email = value!;
+                    },
+                hintText: 'البريد الإلكتروني',
+                textInputType: TextInputType.emailAddress,
+              ),
+              verticalSpace(16),
+              PasswordField(
+                onSaved: (value) {
+                      password = value!;
+                    },
+              ),
+              verticalSpace(16),
+              TermsAndConditionsWidget(onChanged: (value) {}),
+              verticalSpace(30),
+              CustomButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    if (isTermsAccepted) {
+                      context
+                          .read<SignupCubit>()
+                          .createUserWithEmailAndPassword(
+                            email,
+                            password,
+                            userName,
+                          );
+                    } else {
+                      CustomShowBar(message: 'يجب عليك الموافقة على الشروط والإحكام');
+                    }
+                  } else {
+                    setState(() {
+                      autovalidateMode = AutovalidateMode.always;
+                    });
+                  }
+                },
+                text: 'إنشاء حساب جديد',
+              ),
+              verticalSpace(26),
+              HaveAnAccountWidget(),
+            ],
+          ),
         ),
       ),
     );
