80586349b23c7ee76c5cac1363b9b6a7d0a5c5f2
AmmarYaser <ay1513600@gmail.com>
2025-05-15 01:41:17 +0300

    add the states of signup cubit in the listener of SignupViewBody in the page of SignupViewBodyBlocConsumer (Completing logic of SignupViewBodyBlocConsumer)

--- lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/widgets/custom_show_bar.dart';
 import 'package:fruit_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
@@ -12,8 +13,13 @@ class SignupViewBodyBlocConsumer extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<SignupCubit, SignupState>(
       listener: (context, state) {
-        // TODO: implement listener
-      },
+if (state is SignupSuccess) {
+          Navigator.of(context).pop();
+        }
+
+        if (state is SignupFailure) {
+          CustomShowBar( message: state.message);
+        }      },
       builder: (context, state) {
         return ModalProgressHUD(
           inAsyncCall: state is SignupLoading ? true:false,
