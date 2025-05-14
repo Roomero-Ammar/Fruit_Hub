9ef87cd729dae6a550b7fba28953019ad72f5b2a
AmmarYaser <ay1513600@gmail.com>
2025-05-14 06:22:01 +0300

    create getIt service file , and use it inside bloc builder which wrapped the signup view body by signup cubit

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,17 @@
+import 'package:fruit_hub/core/services/firebase_auth_service.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../features/auth/data/repos/auth_repo_impl.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  getIt.registerSingleton<AuthRepo>(
+    AuthRepoImpl(firebaseAuthService: getIt<FirebaseAuthService>()),
+  );
+
+  // // Alternatively you could write it if you don't like global variables
+  //   GetIt.I.registerSingleton<AppModel>(AppModel());
+}
--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/services/get_it_service.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
+import '../cubits/signup_cubit/cubit/signup_cubit.dart';
 import 'widgets/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -7,9 +11,17 @@ class SignupView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: buildAppBar(context, title: "حساب جديد", showBackButton: true,showNotification: false),
-      body: SignupViewBody(),
+    return Scaffold(
+      appBar: buildAppBar(
+        context,
+        title: "حساب جديد",
+        showBackButton: true,
+        showNotification: false,
+      ),
+      body: BlocProvider(
+        create: (context) => SignupCubit(getIt<AuthRepo>()),
+        child: SignupViewBody(),
+      ),
     );
   }
 }
