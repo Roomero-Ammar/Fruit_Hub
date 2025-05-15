f7da8002d2303c88c2b62a4404bd4dd34f351042
AmmarYaser <ay1513600@gmail.com>
2025-05-15 06:08:25 +0300

    create the service of bloc observer and use it in main to handle the style of errors in debug console

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/services/get_it_service.dart';
 import 'package:fruit_hub/firebase_options.dart';
 import 'package:fruit_hub/fruit_hub.dart';
+import 'core/services/custom_bloc_observer.dart';
 import 'core/services/shared_preferences_singleton.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = CustomBlocObserver();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
