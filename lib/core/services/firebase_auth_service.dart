dee5cc3a574879477390c5df01615d07609e6177
AmmarYaser <ay1513600@gmail.com>
2025-05-18 01:05:31 +0300

    add log errors in FirebaseAuthService , AuthRepoImpl.

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,4 +1,6 @@
 
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../errors/exceptions.dart';
@@ -14,12 +16,18 @@ class FirebaseAuthService
   );
   return credential.user!;
 } on FirebaseAuthException catch (e) {
+  log("Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()} and code is ${e.code}");
   if (e.code == 'weak-password') {
 throw CustomException(message: 'الرقم السري ضعيف جداً.');  } else if (e.code == 'email-already-in-use') {
     throw CustomException(message: 'البريد الالكتروني مستخدم بالفعل.');
-  }else{
-    throw CustomException(message: 'An Error occured. Please try again.');}
+  }
+  else if (e.code == 'network-request-failed') {
+        throw CustomException(message: 'تاكد من اتصالك بالانترنت.');
+      }
+  else{
+    throw CustomException(message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.');}
 } catch (e) {
+  log("Exception in FirebaseAuthService.createUserWithEmailAndPassword: ${e.toString()}");
   throw CustomException(message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.');
 }
    }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,28 +1,43 @@
+import 'dart:convert';
+import 'dart:developer';  // responible for logging errors "log"
+
 import 'package:dartz/dartz.dart';
 import 'package:fruit_hub/core/errors/exceptions.dart';
-
 import 'package:fruit_hub/core/errors/failures.dart';
-
-import 'package:fruit_hub/features/auth/domain/entites/user_entity.dart';
-
 import '../../../../core/services/firebase_auth_service.dart';
+import '../../domain/entites/user_entity.dart';
 import '../../domain/repos/auth_repo.dart';
 import '../models/user_model.dart';
 
-class AuthRepoImpl extends AuthRepo {
 
-final FirebaseAuthService firebaseAuthService;
+class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
 
   AuthRepoImpl({required this.firebaseAuthService});
   @override
-  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password, String name) async{
-  try {
-  var user = await  firebaseAuthService.createUserWithEmailAndPassword(email, password);
-  return Right(UserModel.fromFirebaseUser(user));
-} on CustomException catch (e) {
-   return Left(ServerFailure(e.message));
-}catch (e) {
-   return Left(ServerFailure('حدث خطأ ما. الرجاء المحاولة مرة اخرى.',));
-  }
+  /// Creates a new user with the provided email, password, and name
+  ///
+  /// Returns a [Right] with a [UserEntity] if the operation is successful, or a
+  /// [Left] with a [Failure] if not. If a [CustomException] is thrown, it will
+  /// be converted to a [ServerFailure] and returned as a [Left]. 
+  /// Any other error will be converted to a [ServerFailure] with a default message and
+  /// returned as a [Left].
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+    String name,
+  ) async {
+    try {
+      var user = await firebaseAuthService.createUserWithEmailAndPassword(
+        email,
+        password,
+      );
+      return Right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      log("Exception in AuthRepoImpl.createUserWithEmailAndPassword: ${e.toString()}");
+      return Left(ServerFailure(e.message));
+    } catch (e) {
+      return Left(ServerFailure('حدث خطأ ما. الرجاء المحاولة مرة اخرى.'));
+    }
   }
 }
