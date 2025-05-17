d35417d7e99f101295e30069ea0025a911b2114b
AmmarYaser <ay1513600@gmail.com>
2025-05-18 01:03:58 +0300

    add simple comment in AuthRepo

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -3,6 +3,10 @@ import 'package:fruit_hub/core/errors/failures.dart';
 import 'package:fruit_hub/features/auth/domain/entites/user_entity.dart';
 
 abstract class AuthRepo {
+  /// Creates a new user in the authentication system with the given 
+  /// [email],[password], and [name].
+  ///
+  /// Returns a [UserEntity] on success, or a [Failure] on failure.
   Future<Either<Failure,UserEntity>> createUserWithEmailAndPassword(
     String email,
     String password,
