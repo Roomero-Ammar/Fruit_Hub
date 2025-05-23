cb6e3228256d4adde7ee43acbc311d0b8346c144
AmmarYaser <ay1513600@gmail.com>
2025-05-23 05:55:13 +0300

    wrap the login_view by signin cubit

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 
+import '../../../../core/services/get_it_service.dart';
+import '../../domain/repos/auth_repo.dart';
+import '../cubits/signin_cubit/signin_cubit.dart';
 import 'widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
@@ -8,9 +12,12 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: buildAppBar(context, title: "تسجيل الدخول", showBackButton: true,showNotification: false),
-      body: LoginViewBody(),
+    return  BlocProvider(
+      create: (context) => SigninCubit(getIt.get<AuthRepo>()),
+      child: Scaffold(
+          appBar: buildAppBar(context, title: "تسجيل الدخول", showBackButton: true,showNotification: false),
+          body: LoginViewBody(),
+        ),
     );
   }
 }
