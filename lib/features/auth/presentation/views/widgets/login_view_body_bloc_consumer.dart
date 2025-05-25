a760e64ecac418453ac386a572f3edc1eea057fb
AmmarYaser <ay1513600@gmail.com>
2025-05-25 07:03:56 +0300

    add page to navigate on after state success

--- lib/features/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/Routing/routes.dart';
+import 'package:fruit_hub/core/helper_functions/extentsions.dart';
 import 'package:fruit_hub/core/widgets/build_show_bar.dart';
+import 'package:fruit_hub/features/auth/presentation/views/signup_view.dart';
 import '../../../../../core/widgets/custom_progress_hud.dart';
 import '../../cubits/signin_cubit/signin_cubit.dart';
 import 'login_view_body.dart';
@@ -15,6 +18,14 @@ class LoginViewBodyBlocConsumer extends StatelessWidget {
     return BlocConsumer<SigninCubit, SigninState>(
       listener: (context, state) {
         if (state is SigninSuccess) {
+          print('Logged in Done');
+
+
+//           Navigator.of(context).push(
+//   MaterialPageRoute(builder: (_) => const SignupView()),
+// );
+
+          context.pushNamed(Routes.signUpView);
           // Navigator.pushNamed(context, MainView.routeName);
         }
 
